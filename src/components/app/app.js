7555cbc9abbfd5ee117c1ff67dcd21e0e2aedc63
Victor <VPolazau@gmail.com>
2022-01-15 21:34:54 +0300

    style

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import Footer from '../footer'
 import Header from '../header'
+import ItemList from '../item-list'
 
 import 'bootswatch/dist/sketchy/bootstrap.css'
 
-
 import './app.css'
+
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+
+      <ItemList />
+
       <Footer />
     </div>
   )
