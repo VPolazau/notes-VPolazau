252e6947212985d6da1b413edd05fd4f014c72c5
Victor <VPolazau@gmail.com>
2022-01-15 19:50:54 +0300

    Header add

---
+++ src/components/header/header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import SearchPanel from '../search-panel'
+
+import './header.css'
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <h1> Notes </h1>
+
+        {/* <input
+          type="color"
+          className="form-control form-control-color"
+          id="exampleColorInput"
+          value="#563d7c"
+          title="Choose your color"
+        ></input> */}
+
+        <button type="button" className="btn btn-outline-secondary btn-new-note">
+           New note
+        </button>
+
+        <SearchPanel />
+      </div>
+    </nav>
+  )
+}
+
+export default Header
