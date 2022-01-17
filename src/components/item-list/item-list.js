e0a24d759cd0a045a44e0d35b9c1b1932dda7122
Victor <VPolazau@gmail.com>
2022-01-17 13:52:49 +0300

    rows setting

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -15,14 +15,14 @@ export default class ItemList extends Component {
   // data[0].note.text = 12312312312
   // console.log(data[0].note.text)
 
-  itemsView = this.data.map((item) => {
+  itemsView = this.data.map(item => {
     const {
       note: { text, tag },
       id,
     } = item
 
     const items = (
-      <div className="col-lg-4" key={id}>
+      <div className='col-lg-4' key={id}>
         <Item text={text} tag={tag} />
       </div>
     )
@@ -31,10 +31,8 @@ export default class ItemList extends Component {
 
     if ((items.key + 1) % 3 === 0 && items.key > '0') {
       const row = (
-        <div className="row" key={items.key / 3}>
-          {this.rowArr[0]}
-          {this.rowArr[1]}
-          {this.rowArr[2]}
+        <div className='row' key={items.key / 3}>
+          {this.rowArr.map(row => row)}
         </div>
       )
       this.key = items.key
@@ -42,23 +40,13 @@ export default class ItemList extends Component {
       return row
     }
 
-    if (this.data.length - 1 - this.key === 2) {
-      const row = (
-        <div className="row" key={items.key / 3}>
-          {this.rowArr[this.key + 1]}
-          {this.rowArr[this.key + 2]}
-        </div>
-      )
-      this.rowArr = []
-      return row
-    }
-
-    if (this.data.length - 1 - this.key === 1) {
-      return (
-        <div className="row" key={items.key / 3}>
-          {this.rowArr[0]}
-        </div>
-      )
+    if (this.data.length - 1 - this.key < 3) {
+      if (this.data.length - items.key === 2) return
+        return (
+          <div className='row' key={items.key / 3}>
+            {this.rowArr.map(row => row)}
+          </div>
+        )
     }
   })
 
