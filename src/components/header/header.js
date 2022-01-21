41937d40d0b20bd0ff7309c72fe25ec5b25033be
Victor <VPolazau@gmail.com>
2022-01-21 15:38:43 +0300

    phone screen apdate

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ import './header.css'
 const Header = ({newNoteBtn, onSearchChange}) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      <div className="container-fluid header-container">
         <h1> Notes </h1>
         <button type="button" className="btn btn-outline-secondary btn-new-note"
         onClick={newNoteBtn}>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -2,29 +2,27 @@ import React, { Component } from 'react'
 
 import './search-panel.css'
 
-export default class SearchPanel extends Component{
+export default class SearchPanel extends Component {
   state = {
     term: '',
   }
 
-  onSearchChange = (e) => {
+  onSearchChange = e => {
     const term = e.target.value.toLowerCase()
-    this.setState({term})
+    this.setState({ term })
     this.props.onSearchChange(term)
   }
 
-  render(){
+  render() {
     return (
-      <div className="collapse navbar-collapse" id="navbarColor03">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item"></li>
-        </ul>
-        <form className="d-flex">
+      <div className='collapse navbar-collapse' id='navbarColor03'>
+        <ul className='navbar-nav me-auto'></ul>
+        <form className='d-flex input-tag'>
           <input
-            className="form-control me-sm-2"
-            type="text"
-            placeholder="enter tag"
-            value = {this.state.term}
+            className='form-control me-sm-2'
+            type='text'
+            placeholder='enter tag'
+            value={this.state.term}
             onChange={this.onSearchChange}
           ></input>
         </form>
