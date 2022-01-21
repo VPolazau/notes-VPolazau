eb8b82d37b68daec971b38fdf84fe5ba9839ab53
Victor <VPolazau@gmail.com>
2022-01-21 13:18:20 +0300

    _transformData()

--- src/service/service.js
+++ src/service/service.js
@@ -1,9 +1,16 @@
-import * as Data from '../data/data.json';
+import * as Data from '../data/data.json'
 
-export default class Service{
-  data = Data;
+export default class Service {
+  data = Data
 
-  getData(){
-    return this.data.notes
+  getData() {
+    return this._transformData().notes
+  }
+
+  _transformData() {
+    const _re = /\Ï/gm
+    const strData = JSON.stringify(this.data)
+    const newDataStr = strData.replace(_re, '#')
+    return JSON.parse(newDataStr)
   }
 }
