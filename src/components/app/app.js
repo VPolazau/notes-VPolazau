4fac7bcadcf5cb90cf8726d17732348e650bd10b
Victor <VPolazau@gmail.com>
2022-01-21 13:18:39 +0300

    saveData()

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -58,9 +58,9 @@ export default class App extends Component {
     })
   }
 
-  saveNote = (newDataMod) => {
+  saveNote = newDataMod => {
     this.setState(({ data }) => {
-      const idx = data.findIndex((el) => el.id === newDataMod.id)
+      const idx = data.findIndex(el => el.id === newDataMod.id)
 
       const newArray = [
         ...data.slice(0, idx),
@@ -68,7 +68,6 @@ export default class App extends Component {
         ...data.slice(idx + 1),
       ]
 
-      // this.saveData(newArray, 'data.json', '../../data/data.json')
       this.saveData(newArray)
 
       return { data: newArray, modView: false }
@@ -85,17 +84,17 @@ export default class App extends Component {
     })
   }
 
-  modNote = (id) => {
+  modNote = id => {
     this.setState(({ data }) => {
-      const idx = data.findIndex((el) => el.id === id)
+      const idx = data.findIndex(el => el.id === id)
 
       return { modView: true, dataMod: data[idx] }
     })
   }
 
-  deleteItem = (id) => {
+  deleteItem = id => {
     this.setState(({ data }) => {
-      const idx = data.findIndex((el) => el.id === id)
+      const idx = data.findIndex(el => el.id === id)
 
       const newArray = [...data.slice(0, idx), ...data.slice(idx + 1)]
 
@@ -103,7 +102,7 @@ export default class App extends Component {
     })
   }
 
-  onSearchChange = (term) => {
+  onSearchChange = term => {
     this.setState({ term })
   }
 
@@ -111,43 +110,30 @@ export default class App extends Component {
     if (term.length === 0) {
       return items
     }
-    return items.filter((item) => {
+    return items.filter(item => {
       return item.note.tag.join('').indexOf(term) > -1
     })
   }
 
-  saveData = (data) => {
-    const dataStr = JSON.stringify(data)
-    const _re = /[^#]*/gm
-    const dataStrSharp = `{"notes":${dataStr.match(_re).join('')}}`
+  saveData = data => {
+    // const dataStr = JSON.stringify(data)
+    // const _re = /\#/gm
+    // const newDataStr = `{"notes":${dataStr.replace(_re, 'Ï')}}`
 
-    var val = dataStr
-    if (dataStr === undefined) {
-      val = ''
-    }
-    const download = document.createElement('a')
-    download.href =
-      'data:text/plain;content-disposition=attachment;filename=file,' + dataStrSharp
-    download.download = 'data.json'
-    download.style.display = 'none'
-    download.id = 'download'
-    document.body.appendChild(download)
-    document.getElementById('download').click()
-    document.body.removeChild(download)
-
-    // 1ый способ не работает
-    //
-    // const a = document.getElementById("save");
-    // const file = new Blob([dataStr], {type: type});
-    // a.href = URL.createObjectURL(file);
-    // a.download = name;
-
-    // 2ой способ тоже не вышел
-    //
-    // const fs = require('fs')
-    // fs.writeFile('data.json', dataStr, (err) => {
-    //   if (err) throw err
-    // })
+    // var val = dataStr
+    // if (dataStr === undefined) {
+    //   val = ''
+    // }
+    // const download = document.createElement('a')
+    // download.href =
+    //   'data:text/uri-list;content-disposition=attachment;filename=file,' +
+    //   newDataStr
+    // download.download = 'data123.json'
+    // download.style.display = 'none'
+    // download.id = 'download'
+    // document.body.appendChild(download)
+    // document.getElementById('download').click()
+    // document.body.removeChild(download)
   }
 
   render() {
@@ -165,14 +151,14 @@ export default class App extends Component {
     ) : null
 
     return (
-      <div className="container">
+      <div className='container'>
         <Header
           newNoteBtn={this.newNote}
           onSearchChange={this.onSearchChange}
         />
 
         {modifier}
-        <div className="content">
+        <div className='content'>
           <ItemList
             data={visibleNotes}
             onDeleted={this.deleteItem}
